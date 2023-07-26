c594252b2e3553c52648f2c70bdd1a24404c25ca
AnneEberhard <anne.eberhard@gmx.net>
2023-07-26 19:36:44 +0200

    added fonts and first layout affTask

---
+++ js/add_task.js
@@ -0,0 +1,25 @@
+async function initTask() {
+    await includeHTML();
+}
+
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html"); 
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
+
+function addSubTask() {
+    //fill field subTasks
+}
+
+function assignPrio(prio) {
+    //assign Prio
+}
