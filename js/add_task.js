2aa25b0a65f87ee4fee0fffed860c3412a9f93f3
A. E <you@example.com>
2023-07-29 20:40:00 +0200

    new category template added

--- js/add_task.js
+++ js/add_task.js
@@ -1,6 +1,13 @@
 let tasks = [];
 let prios = ['urgent', 'medium', 'low'];
-let categories = ['Backoffice', 'Design', 'Marketing', 'Media', 'Sales'];
+//let categ = ['New category','Backoffice', 'Design', 'Marketing', 'Media', 'Sales'];
+let categories = [
+  {'name': 'New category', 'colorCode': 0},
+  {'name': 'Backoffice', 'colorCode': 1},
+  {'name': 'Design', 'colorCode': 2},
+  {'name': 'Media', 'colorCode': 3},
+  {'name': 'Sales', 'colorCode': 4},
+];
 let contacts = ['Anton Mayer', 'Anja Schulz'];
 let assignedPrio;
 let subTasksArray = [];
@@ -28,12 +35,18 @@ async function includeHTML() {
 function renderCategories() {
   document.getElementById('categoryOptions').innerHTML = '';
   for (let i = 0; i < categories.length; i++) {
-    const category = categories[i];
+    const category = categories[i]['name'];
+    if (i==0) {
+      document.getElementById('categoryOptions').innerHTML += /*html*/`
+      <div id="category${i}" class="option" onclick="addNewCategory()">
+      <div>${category}</div></div>
+    </div>`;
+    } else {
     document.getElementById('categoryOptions').innerHTML += /*html*/`
   <div id="category${i}" class="option" onclick="selectCategory(${i})">
   <div>${category}</div>
   <div class="circle colorCategory${i}"></div>
-</div>`;
+</div>`;}
   }
 }
 
@@ -42,11 +55,15 @@ function renderContacts() {
   for (let i = 0; i < contacts.length; i++) {
     const contact = contacts[i];
     document.getElementById('contactsOptions').innerHTML += /*html*/`
-  <div id="contact${i}" class="options contactList" onclick="selectContact(${i})">
+  <div id="contact${i}" class="options contactList">
   <div>${contact}</div>
-  <div class="checkBox"></div>
+  <div class="checkBox hover" id="contactCheckBox${i}" onclick="assignContact(${i})"></div>
 </div>`;
   }
+}
+
+function assignContact(i) {
+console.log('contactCheckBox', i)
 }
 
 function toggleOptions(id) {
@@ -54,10 +71,65 @@ function toggleOptions(id) {
   optionsDiv.classList.toggle('hidden');
 }
 
-function selectOption(option) { //noch bearbeiten
-  const selectedOptionDiv = document.querySelector('.selected-option');
-  selectedOptionDiv.textContent = option;
-  toggleOptions(); 
+function selectCategory(i) {
+  const category = categories[i]['name'];
+ document.getElementById('selection').innerHTML = templateSelectedCategory(i, category);
+  toggleOptions('categoryOptions');
+}
+
+function templateSelectedCategory(i, category) {
+  let templateSelectedCategory = /*html*/ `  
+  <div class='selectedCategoryBox'>
+    <div id="category${i}" class="selectedCategory" onclick="selectCategory(${i})">
+      <div>${category}</div>
+      <div class="circle colorCategory${i}"></div>
+    </div>
+    <img src="assets/img/down-30.png" onclick="toggleOptions('categoryOptions')"/>
+ </div>`;
+ return templateSelectedCategory;
+}
+
+function addNewCategory() {
+  document.getElementById('selection').innerHTML = templateCreateNewCategory();
+  document.getElementById('newCategoryDotsContainer').innerHTML = templateNewCategoryDots();
+  toggleOptions('categoryOptions');
+}
+
+function templateCreateNewCategory() {
+ let templateCreateNewCategory = /*html*/ `  <div class='selectedCategoryBox'>
+ <div  class="selectedCategory">
+   <input id="newCategoryName" placeholder="New category name">
+ </div>
+ <div class="iconsNewCategory">
+ <img src="assets/img/x.png" onclick="clearCategory()"/>
+ <div class="dividerSmall"></div>
+ <img src="assets/img/done-30.png" onclick="addCategory()"/>
+</div>
+</div>`;
+ return templateCreateNewCategory;
+}
+
+function templateNewCategoryDots() {
+  let templateNewCategoryDots = /*html*/ `<div class="newCategoryDots">
+    <div class="circle colorCategory6 hover" onclick="addColor(6)"></div>
+    <div class="circle colorCategory7 hover" onclick="addColor(7)"></div>
+    <div class="circle colorCategory8 hover" onclick="addColor(8)"></div>
+    <div class="circle colorCategory9 hover" onclick="addColor(9)"></div>
+    <div class="circle colorCategory3 hover" onclick="addColor(3)"></div>
+  </div>`;
+  return templateNewCategoryDots;
+}
+
+function clearCategory() {
+  console.log('clear Category');
+}
+
+function addCategory() {
+  console.log('add Category');
+}
+
+function addColor() {
+  console.log('add Color');
 }
 
 function addSubTask() {
@@ -66,7 +138,7 @@ function addSubTask() {
   let index = findIndexOfItem (subTasksArray, subTask);
   document.getElementById("subTasks").innerHTML += /*html*/ `
     <div class="subTaskBox">
-        <div id="checkBox${index}" class="checkBox" onclick="addCheck(${index})"></div>
+        <div id="checkBox${index}" class="checkBox hover" onclick="addCheck(${index})"></div>
         <div class="subtask">${subTask}</div>
     </div>`;
   document.getElementById("inputSubtask").value = "";
