dd55e7219fc4fb5fd9490532bac2f22d86a602b6
A. E <you@example.com>
2023-08-01 18:00:45 +0200

    addTask functionale with validation and popup

--- js/add_task.js
+++ js/add_task.js
@@ -1,4 +1,5 @@
 //these will come from server and will need to be saved at the end
+// if loadItems and save() are enabled, delete these
 let tasks = [];
 let categories = [
   {'name': 'New category', 'colorCode': 0},
@@ -25,8 +26,10 @@ let subTasksArray = [];
 
 async function initTask() {
   await includeHTML();
+  //await loadItems();
   renderCategories();
   renderContacts();
+  renderDueDate();
 }
 
 async function includeHTML() {
@@ -43,10 +46,16 @@ async function includeHTML() {
   }
 }
 
+async function loadItems() {
+  await getItem("tasks"); 
+  await getItem("contacts"); 
+  await getItem("categories"); 
+  await getItem("freeColors"); 
+}
+
 function renderCategories() {
   document.getElementById("newCategoryDotsContainer").innerHTML = "";
   document.getElementById('category').innerHTML = templateCategory();
-  document.getElementById('categorySelection').setAttribute('disabled', 'true');
   renderCategoryOptions();
   resetCategories();
 }
@@ -59,10 +68,10 @@ function templateCategory() {
  <div id="categorySelectionLeft"></div>
  <div id="dividerSmall"></div>
  <div id="categorySelectionRight">
-  <img src="assets/img/down-30.png" class="hover dropdown" onclick="toggleOptions('categoryOptions')"/>
+  <img src="assets/img/dropdown.svg" class="hover" onclick="toggleOptions('categoryOptions')"/>
  </div>
 </div>
-<div class="hidden" id="categoryOptions"></div>
+<div class="hidden roundedBorder" id="categoryOptions"></div>
 </div>`;
   return templateCategory;
 }
@@ -98,14 +107,15 @@ function templateCategoryOptionsFurther(category,i) {
 function toggleOptions(id) {
   const optionsDiv = document.getElementById(`${id}`);
   optionsDiv.classList.toggle('hidden');
+//  document.getElementById('contactAlert').innerHTML='';
 }
 
 function selectCategory(i) {
   const category = categories[i]['name'];
   assignedCategory = category;
- document.getElementById('categorySelection').value = category;
- document.getElementById('categorySelectionCircle').innerHTML = /*html*/ `
- <div class="circle colorCategory${i}"></div>`;
+  document.getElementById('categorySelection').value = category;
+  document.getElementById('categorySelectionCircle').innerHTML = /*html*/ `
+  <div class="circle colorCategory${i}"></div>`;
   toggleOptions('categoryOptions');
 }
 
@@ -116,7 +126,6 @@ function resetCategories () {
 
 function addNewCategory() {
   document.getElementById('categorySelection').value='';
-  document.getElementById('categorySelection').removeAttribute('disabled');
   document.getElementById('categorySelection').setAttribute('placeholder', 'New category Name');
   document.getElementById('categorySelection').setAttribute('onkeydown', 'checkIfNewCategoryReady()');
   document.getElementById('categorySelectionCircle').innerHTML = '';
@@ -132,7 +141,7 @@ function addNewCategory() {
 
 function templateCategorySelectionLeft() {
   let templateCategorySelectionLeft = `
-  <img src="assets/img/x.png" class="hover iconsNewCategory" onclick="renderCategories()"/>`;
+  <img src="assets/img/iconoir_cancel.svg" class="hover" onclick="renderCategories()"/>`;
   return templateCategorySelectionLeft;
 }
 
@@ -153,7 +162,6 @@ function templateNewCategoryDots(i) {
     <div class="circle colorCategory${colorCode} hover" id="newCategoryDot${i}" onclick="addColor(${i})"></div>`;
   return templateNewCategoryDots;
 }
-
 
 function checkIfNewCategoryReady() {
   newCategoryName = document.getElementById('categorySelection').value;
@@ -177,8 +185,6 @@ function addCategory() {
   const newCategoryObject = { 'name': newCategoryName, 'colorCode': newCategoryColor };
   categories.push(newCategoryObject);
   let lastItem = categories.length -1;
-  console.log(categories);
-  console.log(lastItem);
   const indexToRemove = freeColors.indexOf(newCategoryColor);
 if (indexToRemove !== -1) {
   freeColors.splice(indexToRemove, 1);
@@ -188,26 +194,45 @@ if (indexToRemove !== -1) {
   toggleOptions('categoryOptions');
 }
 
-//invite new contact
-//if contact added required gone
 function renderContacts() {
-  document.getElementById('contactContainer').innerHTML = /*html*/`
-  <div class="inputWithList">
-  <input id="contactSelection" class="selection" required disabled placeholder="Select contacts to assign">
-  <img src="assets/img/down-30.png" class="hover dropdown" onclick="toggleOptions('contactsOptions')"/>
-  </div>
-  <div class="hidden" id="contactsOptions"></div>`;
+  document.getElementById('contactContainer').innerHTML = templateContactSelection();
   for (let i = 0; i < contacts.length; i++) {
     const contact = contacts[i];
-    document.getElementById('contactsOptions').innerHTML += /*html*/`
-      <div class="option contactList">
-        <div id="contact${i}">${contact}</div>
-        <div class="checkBox hover" id="contactCheckBox${i}" onclick="checkContact(${i})"></div>
-      </div>`;
+    document.getElementById('contactsOptions').innerHTML += templateContactsOptions(contact, i);
   }
+  document.getElementById('contactsOptions').innerHTML += templateNewContact();
+}
+
+function templateContactSelection() {
+  let templateContactSelection = /*html*/`
+  <div class="inputWithList">
+  <input id="contactSelection" class="selection" required disabled placeholder="Select contacts to assign">
+  <img src="assets/img/dropdown.svg" class="hover" onclick="toggleOptions('contactsOptions')"/>
+  </div>
+  <div class="hidden roundedBorder" id="contactsOptions"></div>`;
+  return templateContactSelection;
+}
+
+function templateContactsOptions(contact,i) {
+  let templateContactsOptions = /*html*/`
+  <div class="option contactList">
+    <div id="contact${i}">${contact}</div>
+    <div class="checkBox hover" id="contactCheckBox${i}" onclick="checkContact(${i})"></div>
+  </div>`;
+  return templateContactsOptions;
+}
+
+function templateNewContact() {
+  let templateNewContact = /*html*/`
+  <div class="option contactList">
+    <div id="newContact">Invite new contact</div>
+    <div class="newContact roundedBorder"><img src="assets/img/Icon_Contacts_white.png" onclick="inviteContact()"></div>
+  </div>`;
+  return templateNewContact;
 }
 
 function checkContact(i) {
+  debugger;
   if (assignedContactsStatus[i]==false) {
     assignContact(i);
   } else {
@@ -230,16 +255,33 @@ function unassignContact(i) {
 
 function updateAssignedContacts() {
   assignedContacts = [];
+  document.getElementById('contactAlert').innerHTML = '';
   for (let i = 0; i < assignedContactsStatus.length; i++) {
     const contact = contacts[i];
     const assignedStatus = assignedContactsStatus[i];
-    if (assignedStatus==true) {
+    if (assignedStatus == true) {
       assignedContacts.push(contact);
     }
   }
+  if (!assignedContacts.includes(true)) {
+    document.getElementById('contactSelection').removeAttribute('required');
+  } else {
+    document.getElementById('contactSelection').setAttribute('required', 'true');
+  }
 }
 
-//add getDate function and assign mm/yyyy" to id="dueDate"
+// MISSING link to contact page
+function inviteContact() {
+  console.log('invite new contact');
+}
+
+function renderDueDate() {
+  let currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+  const day = String(currentDate.getDate()).padStart(2, '0');
+  document.getElementById('dueDate').setAttribute('min', `${year}-${month}-${day}`);
+}
 
   function renderPrio() {
     document.getElementById("prioContainer").innerHTML = /*html*/`
@@ -256,6 +298,7 @@ function updateAssignedContacts() {
 
 
 function assignPrio(chosenPrio) {
+  document.getElementById('prioAlert').innerHTML ='';
   let capitalPrio = chosenPrio.charAt(0).toUpperCase() + chosenPrio.slice(1);
   assignedPrio = chosenPrio;
   for (let i = 0; i < prios.length; i++) {
@@ -289,7 +332,7 @@ function findIndexOfItem (array, item) {
 }
 
 function addCheck(index) {
-    document.getElementById(`checkBox${index}`).innerHTML = /*html*/ `<img src="../assets/img/done-30.png">`;
+    document.getElementById(`checkBox${index}`).innerHTML = /*html*/ `<img src="assets/img/done-30.png">`;
 }
 
 
@@ -297,42 +340,80 @@ function clearTask() {
   document.getElementById("title").value = "";
   document.getElementById("description").value = "";
   document.getElementById("categoryOptions").innerHTML = "";
+  document.getElementById("categoryAlert").innerHTML = "";
+  document.getElementById("contactAlert").innerHTML = "";
   document.getElementById("dueDate").value = "";
   document.getElementById("inputSubtask").value = "";
   document.getElementById("subTasks").innerHTML = "";
   document.getElementById("urgent").classList.remove("urgent");
   document.getElementById("medium").classList.remove("medium");
   document.getElementById("low").classList.remove("low");
+  document.getElementById("popupNotice").classList.remove("visible");
   renderCategories();
   renderContacts();
   renderPrio();
-  chosenPrio = "";
+  assignedPrio = "";
   subTasksArray = [];
 }
 
 
 function createTask(event) {
   event.preventDefault();
-  let title = document.getElementById("title").value;
-  let description = document.getElementById("description").value;
-  let dueDate = document.getElementById("dueDate").value;
-  let task = {
-      'title': title,
-      'description': description,
-      'category': assignedCategory,
-      'assignedContacts': assignedContacts,
-      'dueDate': dueDate,
-      'prio': assignedPrio,
-      'subtasks': subTasksArray,
-        }
-    tasks.push(task);
-    console.log(task);
-    saveTask();
+  let prioFilled = checkPrio();
+  let correctCategory = checkCorrectCategory();
+  let correctContact = checkCorrectContact();
+  if (prioFilled == true && correctCategory == true && correctContact == true){
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("description").value;
+    let dueDate = document.getElementById("dueDate").value;
+    let task = {
+        'title': title,
+        'description': description,
+        'category': assignedCategory,
+        'assignedContacts': assignedContacts,
+        'dueDate': dueDate,
+        'prio': assignedPrio,
+        'subtasks': subTasksArray,
+          }
+      tasks.push(task);
+      popUpNotice();
+      //saveTask();
+  }
 }
 
-function saveTask() {
+function checkPrio() {
+  if (typeof assignedPrio !== 'undefined' && assignedPrio !== null && assignedPrio !== '') {
+    return true;
+  } else {
+   document.getElementById('prioAlert').innerHTML ='Please select a priority!';
+  }
+}
+
+function checkCorrectCategory() {
+  let inputCategory = document.getElementById("categorySelection").value;
+  const categoryExists = categories.some(category => category.name === inputCategory);
+  if (categoryExists) {
+    return true;
+  } else {
+    document.getElementById('categoryAlert').innerHTML ='Please enter a valid category or choose from the dropdown Menu';
+  }
+}
+
+function checkCorrectContact() {
+  if (assignedContacts.length != 0) {
+    return true;
+  } else {
+    document.getElementById('contactAlert').innerHTML ='Please choose an option from the dropdown Menu';
+  }
+}
+
+function popUpNotice() {
+  document.getElementById('popupNotice').classList.add('visible');
+}
+
+async function saveTask() {
   //save to Server
-  // tasks;
-  // categories;
-  // freeColors;
+  await setItem("tasks", JSON.stringify(tasks));
+  await setItem("categories", JSON.stringify(tasks));
+  await setItem("freeColors", JSON.stringify(tasks));
 }
