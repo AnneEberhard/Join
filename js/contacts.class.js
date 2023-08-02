09d049c6a96b53cbcf5a0ed967513e59628696c0
Alen Alduk <youstylez@gmail.com>
2023-08-02 21:50:53 +0200

    added Contacts Class

---
+++ js/contacts.class.js
@@ -0,0 +1,11 @@
+class Contact {
+  user_name;
+  phone;
+  email;
+
+  constructor(name, phone, email) {
+    this.user_name = name;
+    this.phone = phone;
+    this.email = email;
+  }
+}
---
+++ js/contacts.js
@@ -0,0 +1,8 @@
+contacts = [
+  new Contact("Anja Schulz", +4917672446077, "schulz@gmail.com"),
+  new Contact("Alen Alduk", +4917672446077, "alen-1997@hotmail.de"),
+  new Contact("Anne Eberhard", +4917672446077, "anne.e@gmail.com"),
+  new Contact("Klemens Naue", +4917672446077, "klemens.n@gmail.com"),
+];
+
+console.log(contacts[2]);
