0f057be4dcfde2e0bee3256f728b8d59aa423b7d
Alen Alduk <youstylez@gmail.com>
2023-08-03 13:05:10 +0200

    added add contacts modal

--- js/contacts.js
+++ js/contacts.js
@@ -6,3 +6,26 @@ contacts = [
 ];
 
 console.log(contacts[2]);
+
+// Code for Letters in Contacts:
+
+{
+  /* <div id="beginn_d" class="contact_list_letter_container">
+        <div class="letter">D</div>
+      </div>
+
+Code for Line divider: 
+<div class="contact_list_stroke"></div>
+
+Code for mini Contact Card:
+
+<div id="contact_list_name_2" class="contact_list_name_container">
+        <div class="contact_list_name_container_inner">
+          <div class="contact_list_name_icon">DH</div>
+          <div class="contact_list_name_mail">
+            <div class="contact_list_name">Danny Herzog</div>
+            <div class="contact_list_mail">HerzogD@gmail.com</div>
+          </div>
+        </div>
+      </div> */
+}
