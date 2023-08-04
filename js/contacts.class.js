bf6e4e24ccf9dba599d23cb9272ce62095176928
Alen Alduk <youstylez@gmail.com>
2023-08-04 16:22:47 +0200

    added Contact List render function

--- js/contacts.class.js
+++ js/contacts.class.js
@@ -2,10 +2,12 @@ class Contact {
   user_name;
   phone;
   email;
+  acronym;
 
-  constructor(name, phone, email) {
+  constructor(name, phone, email, acronym) {
     this.user_name = name;
     this.phone = phone;
     this.email = email;
+    this.acronym = acronym;
   }
 }
